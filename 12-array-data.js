730bcd99a997761a5dc57919272a313bd8ae492d
Syedain Iqbal Shigri <sishigree@gmail.com>
2026-02-24 15:04:55 +0500

    array .map

--- 12-array-data.js
+++ 12-array-data.js
@@ -1,8 +1,31 @@
-let arr={
+console.log("=============person================");
+let person1={
     name:"syedain",
     fathername:"miqbal",
     class:"2nd year",
     student:"pscs"
 };
-console.log(arr.fathername);
+console.log(person1.fathername);
+
+
+console.log("=============person2================");
+let person2={
+    name:"ali",
+    fathername:"zeeshan",
+    class:"2nd year",
+    student:"army public school"
+};
+console.log(person2.fathername);
+
+console.log("=============person3================");
+let person3={
+    name:"muhammad",
+    fathername:"hussain",
+    class:"2nd year",
+    student:"degree college "
+};
+console.log(person3.fathername);
+
+let people=[person1, person2, person3]
+people.map()
 
